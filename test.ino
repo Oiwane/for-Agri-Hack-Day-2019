81ddfbbe13e3e76aa57361a748ccce6de39624e1
Oiwane <44086176+Oiwane@users.noreply.github.com>
2019-02-02 19:18:26 +0900

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,35 @@
+const int ledPin = 4;
+const int sensorPin = A4;
+const int threshold = 3.0;
+int value = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  value = analogRead(sensorPin);
+  float voltage = (value/1024.0) * 5.0;
+
+  Serial.print("voltage Value : ");
+  Serial.print(voltage, 4);
+  Serial.println("V");
+
+  //float weight = 0;
+
+  if(voltage > threshold){
+    //weight = 0.0229 * voltage - 1.3372;
+    //if(weigth < 0) weight = 0;
+    digitalWrite(ledPin, HIGH);
+  }else{
+    digitalWrite(ledPin, LOW);
+  } 
+  //Serial.print(" ,weight value : ");
+  //Serial.print(weight, 4);
+  //Serial.println("g");
+  
+  delay(1000);
+}
